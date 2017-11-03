7e9dd72248b199b8420679da4f4f18c95a073d94
Rahul <ietrahul@gmail.com>
2017-11-03 23:49:53 +0530

    Check wining logic and pointer events auto none

--- simongame.js
+++ simongame.js
@@ -1,9 +1,11 @@
-var score = 5, seq = [], myVar, start =0, prevExampleId;
-$(document).ready(function() {
+var score = 1, seq = [], myVar, start =0, prevExampleId, timer, count =0;
+var scoreElementId;
 
-$('#btn-toggle').change(function() {
+$(document).ready(function() {
+  $('#btn-toggle').change(function() {
         if (document.getElementById('btn-toggle').checked) {
            // console.log("i m here--");
+            scoreElementId = document.getElementById("score");
             $(".score").html("- -");
         }
     });
@@ -16,28 +18,7 @@ function displaySound(currentBtn, audioId, exampleId) {
         document.getElementById(audioId).pause();
        // player.currentTime = 0;
     }, 3000);*/
-    //document.getElementById(audioId).play();
     var property = document.getElementById(currentBtn);
-    //var OldBtnColor = property.style.backgroundColor ;
-    //console.log("OldBtnColor:" + OldBtnColor);
-    /*var btnColor;
-    switch(currentBtn) {
-    case "btn1":
-        btnColor = "#0a5ef4";
-        break;
-    case "btn2":
-        btnColor = "#FFFFFF";
-        break;
-    case "btn3":
-        btnColor = "#FFFFFF";
-        break;
-    case "btn4":
-        btnColor = "#FFFFFF";
-        break;
-    default:
-        btnColor = "#FFFFFF";
-};
-  console.log("bg backgroundColor:" + btnColor);*/
        prevExampleId = exampleId;
        property.style.WebkitAnimationName = exampleId;
        property.style.animationDuration = '3s';
@@ -47,20 +28,15 @@ function startGame(){
    // console.log("In startGame:" + score.value);
   //reset seq
   seq = [];
-  document.getElementById("score").innerHTML = "01";
-  for (i=0; i<5; i++){
+  scoreElementId.innerHTML = "01";
+  for (i=0; i<20; i++){
    //var num = Math.floor((Math.random() * 4) + 1);
    seq.push(Math.floor((Math.random() * 4) + 1));
   }
   console.log(seq);
 
-myVar = setInterval(playNow, 5000);
+myVar = setInterval(playNow, 4000);
 
-  //document.getElementById('audio1').play();
-  //setInterval(change, 1000);
-  //var property = document.getElementById('btn1');
-  //console.log("bg backgroundColor:" + property.style.backgroundColor);
-    //        property.style.backgroundColor = "#0a5ef4";
 }
 
 function playNow(){
@@ -81,12 +57,56 @@ function playNow(){
     start +=1;
     displaySound(currentBtn, audioId, exampleId);
     } else {
-     //setInterval(displaySound(currentBtn, audioId), 2000);
-     clearInterval(myVar);
-     start = 0;
+        console.log("turn count --"+ start)
+        // btnElements = document.getElementsByClassName('btn');
+        document.getElementById("btn" + seq[start-1]).style.WebkitAnimationName ="";
+        document.getElementById("btn" + seq[start-1]).style.animationDuration ="";
+        document.getElementById('btn1').style.pointerEvents = "auto";
+        document.getElementById('btn2').style.pointerEvents = "auto";
+        document.getElementById('btn3').style.pointerEvents = "auto";
+        document.getElementById('btn4').style.pointerEvents = "auto";
+       //timer = setTimeout(5000, lost());
+       //clearIntervadocument.getElementsByClassName('btn')l(myVar);
+       //start = 0;
     }
-
-  //}
 }
 
+function validate(id){
+    console.log("validate id--" + id +"--seq[count]--"+seq[count]);
+    //clearTimeout(timer);
+    if (id === seq[count]) {
+      count +=1;
+        if (count == score){
+          score += 1;
+          count = 0;
+          start = 0;
+          scoreElementId.innerHTML = score;
+          setPointerEvents("none");
+        }
+      //timer = setTimeout(5000, lost());
+    } else {
+        lost();
+    }
+}
+
+function lost(){
+    console.log("lost");
+   clearInterval(myVar);
+   //clearTimeout(timer);
+   //console.log('scoreElementId:' + scoreElementId);
+   document.getElementById("audio4").play();
+   document.getElementById("audio1").play();
+   scoreElementId.innerHTML = "! !";
+   score = 1;
+   start = 0;
+   count = 0;
+   setPointerEvents("none");
+}
+
+function setPointerEvents(value){
+    document.getElementById('btn1').style.pointerEvents = value;
+   document.getElementById('btn2').style.pointerEvents = value;
+   document.getElementById('btn3').style.pointerEvents = value;
+   document.getElementById('btn4').style.pointerEvents = value;
+}
 
